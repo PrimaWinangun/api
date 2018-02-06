b9336c2a5015eabd1de69ff72493879d2c4cb9b4
Prima Winangun <primawinangun@gmail.com>
2018-02-06 11:15:31 +0800

    update object

--- objects/product.php
+++ objects/product.php
@@ -77,6 +77,44 @@ class Product{
      
 	}
 	
+	function update(){
+ 
+		// query to insert record
+		$query = "UPDATE
+					" . $this->table_name . "
+				SET
+					name=:name, price=:price, description=:description, category_id=:category_id, created=:updated
+				WHERE
+					id=:id";
+	 
+		// prepare query
+		$stmt = $this->conn->prepare($query);
+	 
+		// sanitize
+		$this->id=htmlspecialchars(strip_tags($this->id));
+		$this->name=htmlspecialchars(strip_tags($this->name));
+		$this->price=htmlspecialchars(strip_tags($this->price));
+		$this->description=htmlspecialchars(strip_tags($this->description));
+		$this->category_id=htmlspecialchars(strip_tags($this->category_id));
+		$this->updated=htmlspecialchars(strip_tags($this->updated));
+	 
+		// bind values
+		$stmt->bindParam(":id", $this->id);
+		$stmt->bindParam(":name", $this->name);
+		$stmt->bindParam(":price", $this->price);
+		$stmt->bindParam(":description", $this->description);
+		$stmt->bindParam(":category_id", $this->category_id);
+		$stmt->bindParam(":updated", $this->updated);
+	 
+		// execute query
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;
+     
+	}
+	
 	// delete the product
 	function delete(){
  
---
+++ product/update.php
@@ -0,0 +1,45 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// get database connection
+include_once '../config/database.php';
+ 
+// instantiate product object
+include_once '../objects/product.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$product = new Product($db);
+ 
+// get posted data
+$json = file_get_contents("php://input");
+$data = json_decode($json);
+ 
+// set product property values
+$product->id = $data->id;
+$product->name = $data->name;
+$product->price = $data->price;
+$product->description = $data->description;
+$product->category_id = $data->category_id;
+$product->updated = date('Y-m-d H:i:s');
+ 
+// create the product
+if($product->update()){
+    echo '{';
+        echo '"message": "Product was updated."';
+    echo '}';
+}
+ 
+// if unable to create the product, tell the user
+else{
+    echo '{';
+        echo '"message": "Unable to update product."';
+    echo '}';
+}
+?>
